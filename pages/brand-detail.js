50ed42b469c61ff12974753276c1555a6479dc63
Alif Faishol <alifaishol@gmail.com>
2020-04-26 21:32:14 +0700

    brand page

--- pages/brand-detail.js
+++ pages/brand-detail.js
@@ -1,3 +1,38 @@
-const BrandDetail = { template: '<p>Halaman Brand Detail</p>' };
+const BrandDetail = {
+  data: function() {
+    return {
+      data: null,
+      loading: true,
+      error: null,
+    }
+  },
+  template: `
+  <div>
+    <template v-if="!loading && data">
+      <h1 class="title">{{ data.name }}</h1>
+      <p>{{ data.description }}</p>
+    </template>
+    <b-skeleton :active="loading" />
+  </div>
+  `,
+  methods: {
+    loadData: function() {
+      this.loading = true;
+      fetch('/res/brand.json')
+        .then(res => res.json())
+        .then(json => {
+          this.data = json.data.find(brand => brand.id.toString() === this.$route.params.id)
+          this.loading = false;
+        })
+        .catch(err => {
+          this.error = err;
+          this.loading = false;
+        })
+    }
+  },
+  mounted: function() {
+    this.loadData()
+  }
+};
 
 export default BrandDetail;
--- pages/brand.js
+++ pages/brand.js
@@ -1,3 +1,73 @@
-const Brand = { template: '<p>Halaman Brand</p>' };
+const NEWS_API_KEY = 'db9b661f07bd4234917209689296ee31'
 
-export default Brand
+const BrandCard = {
+  props: ['brand'],
+  template: `
+  <router-link :to="{ path: '/brand/' + brand.id }">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img :src="brand.image || 'https://source.unsplash.com/random'" alt="Placeholder image" style="object-fit: cover">
+        </figure>
+      </div>
+      <div class="card-header">
+        <div class="card-header-title">{{ brand.name }}</div>
+      </div>
+    </div>
+  </router-link>
+  `
+}
+
+
+const Brand = {
+  data: function() {
+    return {
+      data: [],
+      page: 1,
+      totalPage: 1,
+      loading: true,
+      error: null,
+      styles: {
+        brandContainer: {
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, 300px)',
+          gridGap: '1rem',
+          justifyContent: 'center',
+        }
+      }
+    };
+  },
+  components: {
+    'brand-card': BrandCard
+  },
+  template: `
+  <div>
+    <template v-if="!loading">
+      <div :style="styles.brandContainer">
+        <brand-card :brand="brand" v-for="brand in data" />
+      </div>
+    </template>
+    <b-skeleton :active="loading" />
+  </div>
+  `,
+  methods: {
+    loadData: function() {
+      this.loading = true
+      fetch('/res/brand.json')
+        .then(res => res.json())
+        .then(json => {
+          this.data = json.data
+          this.loading = false
+        })
+        .catch(err => {
+          this.error = err
+          this.loading = false
+        })
+    }
+  },
+  mounted: function() {
+    this.loadData()
+  }
+};
+
+export default Brand;
