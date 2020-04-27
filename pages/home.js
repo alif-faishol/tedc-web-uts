1fec5eeaf8fed8d0bfe1fa2b965e875b53159d82
Yusuf Sahidin <yusufsahidin@Yusufs-MacBook-Pro.local>
2020-04-27 13:53:23 +0700

    edit home page

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,17 @@
-const Home = { template: '<p>Halaman Home</p>' };
+const Home = {
+  template: `
+<section class="hero is-medium">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title" style="@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); font-family: 'Lobster', cursive;">
+        Selamat datang
+      </h1>
+      <h2 class="subtitle" style="@import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap'); font-family: 'Lobster', cursive;">
+        Bird Shop
+      </h2>
+    </div>
+  </div>
+</section>`,
+};
 
 export default Home;
